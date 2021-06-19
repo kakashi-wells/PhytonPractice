b532ff4a14b534bd040864554e5bae8a59532e34
kakashi-wells <arthurbedoya99@gmail.com>
2021-06-18 22:00:20 -0500

    Creacion de tiempo.py

---
+++ funciones.py
@@ -0,0 +1,7 @@
+def saludar(name):
+    return "hello {} welcome Sword Art Online"
+    
+    int("Ingresa tu name")
+    nombre = input()
+    print(saludar(name))
+    
---
+++ tiempo.py
@@ -0,0 +1,6 @@
+rep = 1
+while rep <=3:
+    print("message")
+    rep+= 1
+
+    print ("el ciclo se termino")
