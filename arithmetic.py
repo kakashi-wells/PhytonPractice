40a3a74ef5451e15790e70a156954887af05a27f
kakashi-wells <arthurbedoya99@gmail.com>
2021-06-23 01:22:41 -0500

    Añadiendo tiros

--- arithmetic.py
+++ arithmetic.py
@@ -1,10 +1,12 @@
-print("ingresa 1 golpe")
+print("ingresa 1 tiro")
 num1=int(input())
-print("ingresa 2 golpes")
+print("ingresa 2 tiros")
 num2=int(input())
-print("ingresa 3 golpes")
+print("ingresa 3 tiros")
 num3=int(input())
-print("ingresa 4 golpes")
+print("ingresa 4 tiros")
 num4=int(input())
-resultado = num1 + num2 + num3 + num4
+print("ingresa 5 tiros")
+num5=int (input())
+resultado = num1 + num2 + num3 + num4 + num5
 print(resultado)
